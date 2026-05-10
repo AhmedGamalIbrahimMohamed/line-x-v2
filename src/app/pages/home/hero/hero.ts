eb8a5236f6c1bf515017a87bfcdfee758fcc0038
Ahmed Gamal <a.ibrahim@noqoodholding.com>
2026-05-10 19:02:32 +0300

    feat: implement translation for hero section content in multiple languages

--- src/app/pages/home/hero/hero.ts
+++ src/app/pages/home/hero/hero.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
   templateUrl: './hero.html',
   styleUrl: './hero.scss',
+  imports: [TranslateModule]
 })
 export class HeroComponent {}
